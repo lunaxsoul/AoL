77954883ab9fdb08cd5d7326031ba1ea7c6b0936
lunaxsoul <145118121+lunaxsoul@users.noreply.github.com>
2024-12-04 16:09:20 +0700

    Add files via upload

---
+++ AOL/AOL_2/AoL_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// 1.	Menu, Input & Output – 10 points
+// 2.	Data type and algorithm – 15 points
+// 3.	Sorting - 15 points
+// 4.	Read File – 10 points
+// 5.	Write to File – 10 points
+// 6.	Report – 10 points
+
+int main()
+{
+    printf("======================================");
+    puts("");
+    puts("");
+    printf("Welcome to the math quiz game!");
+    puts("");
+    puts("");
+    printf("======================================");
+    puts("");
+    puts("");
+    printf("Here are some rules to play this game");
+    puts("");
+    puts("");
+    printf("There will be questions regarding to mathematics\n");
+    printf("Answer the question and you will get a score if your answer is right\n");
+    puts("");
+    puts("");
+    printf("Ready to start the game?\n");
+    puts("");
+    printf("yes\n");
+    printf("no\n");
+    puts("");
+
+    return 0;
+}
